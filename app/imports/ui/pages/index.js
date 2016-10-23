f544ebaf0c7a2a74dd4c251ed418c8cd74dac453
Justin Chock <jchock25@hawaii.edu>
2016-10-22 23:00:43 -1000

    Finished Login Page

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -4,7 +4,10 @@ import './add-stuff-page.js';
 import './edit-stuff-page.html';
 import './edit-stuff-page.js';
 
-import './home-page.html';
+import './landing-page.html';
+
+import './login-page.html';
+import './login-page.js';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
